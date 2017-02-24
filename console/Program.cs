ceb47527cf36e7eb4c18c586324da695467946ab
何伟庚 <6318275@qq.com>
2017-02-25 00:39:02 +0800

    反射创建实例

---
+++ console/Program.cs
@@ -0,0 +1,29 @@
+﻿using dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name = typeof(IClass1).Namespace;
+                IClass1 ic  = (IClass1)Assembly.Load(name).CreateInstance(name+".Class1");
+                ic._console();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ dal/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal
+{
+    public class Class1:IClass1
+    {
+        public void _console()
+        {
+            Console.WriteLine("反射成功");
+        }
+    }
+}
